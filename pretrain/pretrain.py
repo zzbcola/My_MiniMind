93237f906ed5d060b637d4017fd3816890f68139
zhouzhibiao <zhouzhibiao@gdcattsoft.com>
2026-09-16 23:38:05 +0800

    fix bug in pretrain version 2

--- pretrain/pretrain.py
+++ pretrain/pretrain.py
@@ -40,7 +40,7 @@ def train_epoch(epoch, loader, iters, start_step = 0, wandb = None):
             loss = loss / args.accumulation_steps
 
         # 反向传播
-        scaler.scale(loss).backward() # 使用 GradScaler 放大 loss 后进行反向传播，防止半精度（FP16）下的梯度下溢出。
+        loss.backward() # MPS 上不使用 CUDA GradScaler，直接使用普通的反向传播
         # 梯度累加控制
             # 当累加到一定的步数的时候，对优化器进行一个真正的参数更新
         if step % args.accumulation_steps == 0:
@@ -81,10 +81,11 @@ def train_epoch(epoch, loader, iters, start_step = 0, wandb = None):
         # 尾部梯度更新
         # 剩余数据不足够进行一次梯度更新的时候，将最后一组数据的结果作为最后的更新结果。
         if last_step > start_step and last_step % args.accumulation_steps != 0:
-            scaler.unscale_(optimizer)
+            # scaler.unscale_(optimizer)
             torch.nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
-            scaler.step(optimizer)
-            scaler.update()
+            # scaler.step(optimizer)
+            # scaler.update()
+            optimizer.step()
             optimizer.zero_grad(set_to_none=True)
 
 
@@ -100,7 +101,7 @@ if __name__ == "__main__":
     parser.add_argument("--learning_rate", type=float, default=5e-4, help="初始学习率")
     parser.add_argument("--device", type=str, default="mps" if torch.backends.mps.is_available() else "cpu", help="训练设备")
     parser.add_argument("--dtype", type=str, default="bfloat16", help="混合精度类型")
-    parser.add_argument("--num_workers", type=int, default=8, help="数据加载线程数")
+    parser.add_argument("--num_workers", type=int, default=0, help="数据加载线程数") # macOS 多进程 DataLoader 可能增加内存和进程启动开销。
     parser.add_argument("--accumulation_steps", type=int, default=8, help="梯度累积步数")
     parser.add_argument("--grad_clip", type=float, default=1.0, help="梯度裁剪阈值")
     parser.add_argument("--log_interval", type=int, default=100, help="日志打印间隔")
@@ -116,6 +117,10 @@ if __name__ == "__main__":
     parser.add_argument("--wandb_project", type=str, default="MiniMind-Pretrain", help="wandb项目名")
     parser.add_argument("--use_compile", default=0, type=int, choices=[0, 1], help="是否使用torch.compile加速（0=否，1=是）")
     args = parser.parse_args()
+    if args.device == "mps" and not torch.backends.mps.is_available():
+        raise RuntimeError(
+            "MPS不可用"
+        )
 
 
     # 1、初始化环境和随机种子
@@ -131,7 +136,7 @@ if __name__ == "__main__":
     device_type = "mps" if "mps" in args.device else "cpu"
     dtype = torch.bfloat16 if args.dtype == "bfloat16" else torch.float16
         # mps训练下不启用autocast
-    autocast_ctx = nullcontext() if device_type in ("mps", "cpu") else torch.cuda.amp.autocast(dtype=dtype)
+    autocast_ctx = nullcontext()
 
     # 4. 配wandb
     wandb = None
@@ -146,7 +151,8 @@ if __name__ == "__main__":
     model, tokenizer = init_model(lm_config=lm_config, from_weight=args.from_weight, device=args.device)
     train_ds = PretrainDataset(args.data_path, tokenizer, max_length=args.max_seq_len)
     train_sampler = DistributedSampler(train_ds) if dist.is_initialized() else None # 如果是分布式训练需要分布式采样训练数据
-    scaler = torch.cuda.amp.GradScaler(enabled=(args.dtype == 'float16'))
+    # scaler = torch.cuda.amp.GradScaler(enabled=(args.dtype == 'float16'))
+    scaler = None # mps下不能使用scaler
     optimizer = optim.AdamW(model.parameters(), lr = args.learning_rate)
 
     # 6、checkpoint恢复训练
@@ -154,7 +160,7 @@ if __name__ == "__main__":
     if ckp_data:
         model.load_state_dict(ckp_data['model'])
         optimizer.load_state_dict(ckp_data['optimizer'])
-        scaler.load_state_dict(ckp_data['scaler'])
+        # scaler.load_state_dict(ckp_data['scaler'])
         start_epoch = ckp_data['epoch']
         start_step = ckp_data.get('step', 0)
 
@@ -171,7 +177,7 @@ if __name__ == "__main__":
         indices = torch.randperm(len(train_ds)).tolist()
         skip = start_step if (epoch == start_epoch and start_step > 0) else 0
         batch_sampler = SkipBatchSampler(train_sampler or indices, args.batch_size, skip) # 将采样的数据合成一个个batch方便后续训练
-        loader = DataLoader(train_ds, batch_sampler = batch_sampler, num_workers=args.num_workers, pin_memory = True)
+        loader = DataLoader(train_ds, batch_sampler = batch_sampler, num_workers=args.num_workers, pin_memory = False) # mps下不需要pinned memory
         if skip > 0:
             Logger(f'Epoch [{epoch + 1}/{args.epochs}]: 跳过前{start_step}个step，从step {start_step + 1}开始')
             train_epoch(epoch, loader, len(loader) + skip, start_step, wandb)
